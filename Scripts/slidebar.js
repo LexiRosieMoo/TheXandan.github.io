34152202fa6416856204e0b93c5eb7a1bc043e26
TheXandan <xandan11@gmail.com>
2015-03-04 15:01:17 -0500

    Slidebar Text Added

--- Scripts/slidebar.js
+++ Scripts/slidebar.js
@@ -1,23 +1,31 @@
 $(document).ready(function () {
+	
+
 				$( "#Games")
 				.mouseover(function() {
-					$("#sidebareffect-blue").transition({x:'200px'});
+						$("#sidebareffect-blue").transition({x:'200px'});
+						$("#sidebar-blue").transition({x:'200px'});
+						
 					}
 				)
 				.mouseout(function() {
 					$("#sidebareffect-blue").transition({x:'0px'});
-				}
+					$("#sidebar-blue").transition({x:'0px'});
+					
+					}
 				);
+				
+				
 				
 				$( "#Stuff")
 				.mouseover(function() {
 					$("#sidebareffect-green").transition({x:'200px'});
-					$("#sidebareffect-green-text").transition({x:'200px'});
+					$("#sidebar-green").transition({x:'200px'});
 					}
 				)
 				.mouseout(function() {
 					$("#sidebareffect-green").transition({x:'0px'});
-					$("#sidebareffect-green-text").transition({x:'200px'});
+					$("#sidebar-green").transition({x:'0px'});
 				}
 				);
 				
