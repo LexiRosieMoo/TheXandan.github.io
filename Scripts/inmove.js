9119b09526d3241c02eb4f5878ba338ef5433db7
TheXandan <xandan11@gmail.com>
2015-03-10 20:24:43 -0400

    Reworking how the sidebar functions
    TODO add better sidebar text

---
+++ Scripts/inmove.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+	$("#sidebareffect-green").delay(1000).transition({x:'0px',y:'-2px'});
+	$("#sidebareffect-blue").delay(1000).transition({x:'2px'});
+	//$("#sidebar").delay(1000).transition({x:'2px'});
+});
--- Scripts/slidebar.js
+++ Scripts/slidebar.js
@@ -1,32 +1,68 @@
 $(document).ready(function () {
-	
+	var blueout = false;
+	var greenout = false;
 
-				$( "#Games")
-				.mouseover(function() {
-						$("#sidebareffect-blue").transition({x:'200px'});
-						$("#sidebar-blue").transition({x:'200px'});
-						
+				$("#Home")
+				.click(function() {
+					if (greenout==true)
+					{
+						$("#sidebareffect-green").transition({x:'0px'});
+						$("#sidebar-green").transition({x:'0px'});
 					}
-				)
-				.mouseout(function() {
-					$("#sidebareffect-blue").transition({x:'0px'});
-					$("#sidebar-blue").transition({x:'0px'});
-					
+					if (blueout==true)
+					{
+						$("#sidebareffect-blue").transition({x:'0px'});
+						$("#sidebar-blue").transition({x:'2px'});
+					}
+					}
+				);
+	
+				$("#Games")
+				.click(function() {
+						if(blueout == false)
+						{
+							blueout = true;
+							$("#sidebareffect-blue").transition({x:'200px'});
+							$("#sidebar-blue").transition({x:'200px'});
+							
+							if (greenout==true)
+							{
+								greenout=false;
+								$("#sidebareffect-green").transition({x:'0px'});
+								$("#sidebar-green").transition({x:'0px'});
+							}
+						}
+						else
+						{
+							blueout=false;
+							$("#sidebareffect-blue").transition({x:'2px'});
+							$("#sidebar-blue").transition({x:'0px'})
+						}
 					}
 				);
 				
-				
-				
-				$( "#Stuff")
-				.mouseover(function() {
-					$("#sidebareffect-green").transition({x:'200px'});
-					$("#sidebar-green").transition({x:'200px'});
+				$("#Stuff")
+				.click(function() {
+						if(greenout == false)
+						{
+							greenout = true;
+							$("#sidebareffect-green").transition({x:'200px'});
+							$("#sidebar-green").transition({x:'200px'});
+							
+							if (blueout==true)
+							{
+								blueout=false;
+								$("#sidebareffect-blue").transition({x:'2px'});
+								$("#sidebar-blue").transition({x:'0px'});
+							}
+						}
+						else
+						{
+							greenout=false;
+							$("#sidebareffect-green").transition({x:'0px'});
+							$("#sidebar-green").transition({x:'0px'});
+						}
 					}
-				)
-				.mouseout(function() {
-					$("#sidebareffect-green").transition({x:'0px'});
-					$("#sidebar-green").transition({x:'0px'});
-				}
 				);
 				
 			});
