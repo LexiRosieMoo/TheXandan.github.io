90cd396f47ea74a92f81843591afcf53c3721f1a
TheXandan <xandan11@gmail.com>
2015-03-16 00:48:08 -0400

    Square Sidebar and Reworking Links

--- Scripts/inmove.js
+++ Scripts/inmove.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$("#sidebareffect-green").delay(1000).transition({x:'0px',y:'-2px'});
-	$("#sidebareffect-blue").delay(1000).transition({x:'2px'});
+	$("#sidebareffect-green").delay(100).transition({x:'0px',y:'-2px'});
+	$("#sidebareffect-blue").delay(100).transition({x:'2px'});
 	//$("#sidebar").delay(1000).transition({x:'2px'});
 });
