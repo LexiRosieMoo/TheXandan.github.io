3212479f53d4b12a60acc953001faf46936a8c1b
TheXandan <xandan11@gmail.com>
2015-03-17 15:17:29 -0400

    Reworked Top Bar on index page

---
+++ Scripts/TopSlide.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+	$("#TopTextgreen").delay(100).transition({x:'1px',y:'-1px'});
+	$("#TopTextblue").delay(100).transition({x:'10px', y:'-5'});
+	//$("#sidebar").delay(1000).transition({x:'2px'});
+});
