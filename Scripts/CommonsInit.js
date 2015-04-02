adf17dd304adcbe8b9add2b2eb392d0123a8dab6
TheXandan <xandan11@gmail.com>
2015-04-02 14:57:40 -0400

    Header has hover

---
+++ Scripts/CommonsInit.js
@@ -0,0 +1,18 @@
+document.getElementById('side').innerHTML='	
+				<div class="side" id="sidebareffect-blue"></div>
+				<div class="column" id="sidebar-blue">
+							<A href = "Games.html">Finished Products </A><br>
+							<A href = #>Prototypes </A><br>
+					</div>
+				<div class="side" id="sidebareffect-green"></div>
+				<div class="column" id="sidebar-green">
+							<A>Word Documents</A>
+					</div>
+				<div class="side" id="sidebar"></div>
+				<div class="column" id="sidebar-text">
+				<A href="index.html" id="Home">Home</A> <br>
+				<A id="Games">Games</A> <br>
+				<A id="Stuff">Stuff</A> <br>
+				</div>
+			';
+});
--- Scripts/inmove.js
+++ Scripts/inmove.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
 	$("#sidebareffect-green").delay(100).transition({x:'0px',y:'-2px'});
 	$("#sidebareffect-blue").delay(100).transition({x:'2px'});
-	//$("#sidebar").delay(1000).transition({x:'2px'});
+	/*loop();*/
+	$("#TopTextred").mouseover(function(){
+		$("#TopTextblue").transition({
+			scale:[1.1, 1.1]
+		});
+	
+	
+		$("#TopTextgreen").transition({
+			scale:[1.2, 1.2]
+		});
+		
+		$("#TopTextred").transition({
+			scale:[1.3, 1.3]
+		});
+	}).mouseout(function(){
+		$("#TopTextblue").transition({
+			scale:[1, 1]
+		});
+	
+	
+		$("#TopTextgreen").transition({
+			scale:[1, 1]
+		});
+		
+		$("#TopTextred").transition({
+			scale:[1, 1]
+		});
+	});
 });
